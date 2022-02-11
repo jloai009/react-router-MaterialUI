7da52fad485515d46e59abb28491aeab289930b8
Jose <jloai009@fiu.edu>
2022-02-10 20:54:53 -0500

    Pass Link components as props to Buttons

--- src/index.js
+++ src/index.js
@@ -150,23 +150,21 @@ const App = () => {
     <Container>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-          </IconButton>
-          <Button color="inherit">
-            <Link to="/">home</Link>
+          <Button color="inherit" component={Link} to="/">
+            home
           </Button>
-          <Button color="inherit">
-            <Link to="/notes">notes</Link>
+          <Button color="inherit" component={Link} to="/notes">
+            notes
           </Button>
-          <Button color="inherit">
-            <Link to="/users">users</Link>
+          <Button color="inherit" component={Link} to="/users">
+            users
           </Button>
-          <Button color="inherit">
             {user
               ? <em>{user} logged in</em>
-              : <Link to="/login">login</Link>
+              : <Button color="inherit" component={Link} to="/login">
+                  login
+                </Button>
             }
-          </Button>
         </Toolbar>
       </AppBar>
       <div>
